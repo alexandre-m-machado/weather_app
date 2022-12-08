33ee8ccad126e49882492a2cfcfdb33c0fe0ceb3
Alex <ispg2020100636@ispgaya.pt>
2022-12-08 10:39:50 +0000

    Text Editing Controller, Text field and Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _cityTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      body: Column(children: []),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          const Padding(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              child: SizedBox(
+                  width: 150,
+                  child: TextField(
+                    controller: null,
+                    decoration: InputDecoration(labelText: 'City'),
+                    textAlign: TextAlign.center,
+                  ))),
+          const Padding(
+            padding: EdgeInsets.only(top: 25),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(15.0),
+                fixedSize: Size(150, 50),
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.lightBlue,
+                elevation: 15,
+                shadowColor: Colors.lightBlue,
+                side: const BorderSide(color: Colors.black, width: 2),
+                shape: const StadiumBorder()),
+            child: const Text('Search'),
+          )
+        ],
+      )),
     ));
   }
 }
